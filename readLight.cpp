79a54174ce9a4435b788a39170c8a713d5616c09
Donnie Tolbert <donnie.tolbert@target.com>
2017-01-02 15:15:11 -0600

    added program options

--- readLight.cpp
+++ readLight.cpp
@@ -1,72 +1,13 @@
-/******************************************************************************
-i2ctest.cpp
-Raspberry Pi I2C interface demo
-Byron Jacquot @ SparkFun Electronics>
-4/2/2014
-https://github.com/sparkfun/Pi_Wedge
-
-A brief demonstration of the Raspberry Pi I2C interface, using the SparkFun
-Pi Wedge breakout board.
-
-Resources:
-
-This example makes use of the Wiring Pi library, which streamlines the interface
-to the the I/O pins on the Raspberry Pi, providing an API that is similar to the
-Arduino.  You can learn about installing Wiring Pi here:
-http://wiringpi.com/download-and-install/
-
-The wiringPi SPI API is documented here:
-https://projects.drogon.net/raspberry-pi/wiringpi/spi-library/
-
-The init call returns a standard file descriptor.  More detailed configuration
-of the interface can be performed using ioctl calls on that descriptor.
-See the wiringPi SPI implementation (wiringPi/wiringPiSPI.c) for some examples.
-Parameters configurable with ioctl are documented here:
-http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/Documentation/spi/spidev
-
-Hardware connections:
-
-This file interfaces with the SparkFun Serial 7 Segment display:
-https://www.sparkfun.com/products/11629
-
-The board was connected as follows:
-(Raspberry Pi)(Serial 7 Segment)
-GND  -> GND
-3.3V -> Vcc
-CE1  -> SS (Shift Select)
-SCK  -> SCK 
-MOSI -> SDI
-MISO -> SDO
-
-To build this file, I use the command:
->  g++ spitest.cpp -lwiringPi
-
-Then to run it, first the spi kernel module needs to be loaded.  This can be 
-done using the GPIO utility.
-> gpio load spi
-> ./a.out
-
-This test uses the single-segment mode of the 7 segment display.  It shifts a 
-bit through the display characters, lighting a single character of each at a 
-time.
-
-Development environment specifics:
-Tested on Raspberry Pi V2 hardware, running Raspbian.
-Building with GCC 4.6.3 (Debian 4.6.3-14+rpi1)
-
-This code is beerware; if you see me (or any other SparkFun employee) at the
-local, and you've found our code helpful, please buy us a round!
-
-Distributed as-is; no warranty is given.
-******************************************************************************/
 
 #include <iostream>
 #include <errno.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
+#include <boost/program_options.hpp>
 #include <unistd.h>
 
 using namespace std;
+namespace po = boost::program_options;
 
 // channel is the wiringPi name for the chip select (or chip enable) pin.
 // Set this to 0 or 1, depending on how it's connected.
@@ -74,44 +15,59 @@ static const int CHANNEL = 1;
 
 #define LIGHT_PIN 21
 
-int main()
+int main(int ac, char * av[])
 {
-   int fd, result;
-   unsigned char buffer;
-   int lightPin = LIGHT_PIN;
-   bool lit = false;
+  int fd, result;
+  unsigned char buffer;
+  int lightPin = LIGHT_PIN;
+  bool lit = false;
+  int delay = 1;
 
-   cout << "Initializing" << endl ;
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "produce help message")
+    ("lightPin", po::value<int>(&lightPin)->default_value(LIGHT_PIN), "light pin (BCM)")
+    ("delay", po::value<int>(&delay)->default_value(1), "delay between readings");
 
-   // Configure the interface.
-   // CHANNEL insicates chip select,
-   // 500000 indicates bus speed.
-   wiringPiSetupGpio();
-   digitalWrite(lightPin, LOW);
-   lit = false;
-   fd = wiringPiSPISetup(CHANNEL, 500000);
+  po::variables_map vm;
+  po::store(po::parse_command_line(ac, av, desc), vm);
+  po::notify(vm);
 
-   cout << "Init result: " << fd << endl;
+  if (vm.count("help") > 0) {
+    cout << desc << endl;
+    return 1;
+  }
 
-   // clear display
-   while(true) {
-     wiringPiSPIDataRW(CHANNEL, &buffer, 1);
+  cout << "Initializing" << endl ;
 
-     cout << "value: " << (int)buffer << endl;
+  // Configure the interface.
+  // CHANNEL insicates chip select,
+  // 500000 indicates bus speed.
+  wiringPiSetupGpio();
+  digitalWrite(lightPin, LOW);
+  lit = false;
+  fd = wiringPiSPISetup(CHANNEL, 500000);
 
-     if (buffer < 128 && !lit) {
-       cout << "light on" << endl;
-       digitalWrite(lightPin, HIGH);
-       lit = true;
-     } else if (buffer >= 128 && lit) {
-       cout << "light off" << endl;
-       digitalWrite(lightPin, LOW);
-       lit = false;
-     }
+  cout << "Init result: " << fd << endl;
 
-     sleep(1);
+  // clear display
+  while(true) {
+   wiringPiSPIDataRW(CHANNEL, &buffer, 1);
 
+   cout << "value: " << (int)buffer << endl;
 
+   if (buffer < 128 && !lit) {
+     cout << "light on" << endl;
+     digitalWrite(lightPin, HIGH);
+     lit = true;
+   } else if (buffer >= 128 && lit) {
+     cout << "light off" << endl;
+     digitalWrite(lightPin, LOW);
+     lit = false;
    }
-}
 
+   sleep(1);
+
+
+  }
+}
